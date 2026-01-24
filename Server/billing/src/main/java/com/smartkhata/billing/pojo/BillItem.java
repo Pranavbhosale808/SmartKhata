0410bf0067df9fe50d35110c56a5232fcf35ead0
tejasdhade17 <tejasdhande1703@gmail.com>
2026-01-25 01:15:32 +0530

    updated in pojo

--- Server/billing/src/main/java/com/smartkhata/billing/pojo/BillItem.java
+++ Server/billing/src/main/java/com/smartkhata/billing/pojo/BillItem.java
@@ -1,6 +1,8 @@
 package com.smartkhata.billing.pojo;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,13 +20,23 @@ public class BillItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer itemId;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "invoice_id", nullable = false)
     private Bill bill;
 
+    @NotNull
     private Integer productId;
+
+    @NotBlank
     private String description;
+
+    @NotNull
     private BigDecimal unitPriceSnapshot;
+
+    @NotNull
     private Integer quantity;
+
+    @NotNull
     private BigDecimal lineTotal;
 }
